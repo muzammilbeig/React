fb1a63020ebe4d42603e6c57b1f920f1a9abe75e
Muzammil Beig <muzammilbeig4033@gmail.com>
2023-06-07 10:32:00 +0530

    ....

---
+++ test/src/App.js
@@ -0,0 +1,14 @@
+import TodoList from './TodoList';
+import './App.css';
+
+function App() {
+
+  return (
+
+    <div className="app">
+      <TodoList/>
+    </div>
+  );
+};
+ 
+export default App;
